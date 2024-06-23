0afa0ac926df652d2e34f833fb04df50b23c90bb
aamigo <aamigodeveloper@gmail.com>
2024-06-23 20:50:51 +0330

    fixing some CSS issues

--- app/(root)/_components/Header.tsx
+++ app/(root)/_components/Header.tsx
@@ -1,33 +1,36 @@
 import Image from "next/image"
 import SearchComponent from "./SearchComponent"
 import UserNav from "./UserNav"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <header className="border-b py-6 px-3">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        
-          <Image
-            src={"/airbnb-desktop.png"}
-            alt="logo"
-            width={120}
-            height={100}
-            objectFit="contain"
-            objectPosition="left"
-            className="hidden sm:block"
-          />
-          <Image
-            src={"/airbnb-mobile.webp"}
-            alt="logo"
-            width={45}
-            height={45}
-            objectFit="contain"
-            objectPosition="left"
-            className="sm:hidden"
-          />
+      <Link
+        href={"/"}
+        className="max-w-7xl mx-auto flex items-center justify-between"
+      >
+        <Image
+          src={"/airbnb-desktop.png"}
+          alt="logo"
+          width={120}
+          height={100}
+          objectFit="contain"
+          objectPosition="left"
+          className="hidden sm:block"
+        />
+        <Image
+          src={"/airbnb-mobile.webp"}
+          alt="logo"
+          width={45}
+          height={45}
+          objectFit="contain"
+          objectPosition="left"
+          className="sm:hidden"
+        />
         <SearchComponent />
         <UserNav />
-      </div>
+      </Link>
     </header>
   )
 }
